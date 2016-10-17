5c850265047552dd5cf23910312b481ceb712b76
Nicolas Mauchle <nicolas@nicolasmauchle.ch>
2016-10-17 20:41:25 +0200

    Add correct deal with cards

--- models/Game.js
+++ models/Game.js
@@ -1,5 +1,6 @@
 import RANKS from './../models/Rank';
 
+const card_set = [1, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 5, 6, 6, 6, 6, 6, 6, 7, 7, 7, 7, 7, 7, 7, 8, 8, 8, 8, 8, 8, 8, 8, 9, 9, 9, 9, 9, 9, 9, 9, 9, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 13, 13]
 
 export default class Game {
   constructor(creator, id, {max_player = 4, passcode = "", name = ""}) {
@@ -11,6 +12,25 @@ export default class Game {
     this.finish = false;
     this.started = false;
     this.players = [creator];
+    this.card_set = [];
+  }
+
+  // modern Fisher–Yates shuffle
+  // Link: https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle#The_modern_algorithm
+  create_card_set() {
+    let tmp;
+    for (var i = card_set.length; i-- > 1;) {
+      let j = Math.floor(Math.random() * card_set.length)
+      tmp = card_set[i];
+      card_set[i] = card_set[j];
+      card_set[j] = tmp;
+    }
+    this.card_set = card_set;
+  }
+
+  // Draw a card for the start of the game
+  draw_start() {
+    return Math.floor(Math.random() * 12) + 1;
   }
 
 
@@ -24,17 +44,27 @@ export default class Game {
     let rank = 0;
     this.players.forEach(p => {
       while (assigned_ranks.indexOf(rank) >= 0) {
-        rank = p.draw_start();
+        rank = draw_start();
       }
       p.rank = rank;
-      p.create_card_set()
+      create_card_set()
     });
+    deal_cards();
+  }
+
+  deal_cards() {
+    this.players = this.players.sort((a, b) => a.rank > b.rank);
+    let i = 0;
+    while (this.card_set.length > 0) {
+      this.players[i].cards.push(this.card_set.pop());
+    }
   }
 
   // Start the next round
   next_round() {
     this.players.forEach(p => {
-      p.create_card_set()
+      create_card_set()
+      deal_cards();
     });
   }
 
--- models/Player.js
+++ models/Player.js
@@ -1,5 +1,4 @@
 
-const card_set = [1, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 5, 6, 6, 6, 6, 6, 6, 7, 7, 7, 7, 7, 7, 7, 8, 8, 8, 8, 8, 8, 8, 8, 9, 9, 9, 9, 9, 9, 9, 9, 9, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 13, 13]
 
 export default class Player {
   constructor(id, name, avatar) {
@@ -9,26 +8,8 @@ export default class Player {
     this.points = 0;
     this.game_id = 0;
     this.rank = 0;
-    this.card_set = []
+    this.cards = []
   }
 
-  // modern Fisher–Yates shuffle
-  // Link: https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle#The_modern_algorithm
-  create_card_set() {
-    let tmp;
-    for (var i = card_set.length; i-- > 1;) {
-      let j = Math.floor(Math.random() * card_set.length)
-      tmp = card_set[i];
-      card_set[i] = card_set[j];
-      card_set[j] = tmp;
-    }
-    this.card_set = card_set;
-    return this.card_set;
-  }
-
-  // Draw a card for the start of the game
-  draw_start() {
-    return Math.floor(Math.random() * 12) + 1;
-  }
 
 }
